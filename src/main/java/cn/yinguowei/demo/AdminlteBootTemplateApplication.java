b5f7bfbd32ab4f96ccf8871b4470551c40345b80
yinguowei <yinguowei@gmail.com>
2018-02-22 01:31:22 +0800

    minimize dependencies.

--- src/main/java/cn/yinguowei/demo/AdminlteBootTemplateApplication.java
+++ src/main/java/cn/yinguowei/demo/AdminlteBootTemplateApplication.java
@@ -2,11 +2,21 @@ package cn.yinguowei.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class AdminlteBootTemplateApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AdminlteBootTemplateApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AdminlteBootTemplateApplication.class, args);
+    }
+}
+
+@RestController
+class HelloController {
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello";
+    }
 }
