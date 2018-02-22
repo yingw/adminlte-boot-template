b06278d133f0d2c6035bdf534eb0e71553d8fe8c
yinguowei <yinguowei@gmail.com>
2018-02-22 11:06:05 +0800

    add Webjars dependencies.

--- src/main/java/cn/yinguowei/demo/AdminlteBootTemplateApplication.java
+++ src/main/java/cn/yinguowei/demo/AdminlteBootTemplateApplication.java
@@ -1,9 +1,13 @@
 package cn.yinguowei.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @SpringBootApplication
 public class AdminlteBootTemplateApplication {
@@ -13,10 +17,14 @@ public class AdminlteBootTemplateApplication {
     }
 }
 
-@RestController
+@Controller
 class HelloController {
-    @RequestMapping("/")
-    public String hello() {
-        return "Hello";
+    Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @RequestMapping(value = {"/*.html", "/*/*.html", "/*/*/*.html"})
+    public String hello(HttpServletRequest request) {
+        logger.info("path = {}", request);
+        logger.info("request.getRequestURI() = {}", request.getRequestURI());
+        return request.getRequestURI();
     }
 }
